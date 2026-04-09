f9b5550d2ee4095011b2b6650203855e2d9848b5
thuypy1209 <huymn944@gmail.com>
2026-04-09 08:58:10 +0700

    model create
    chat. chungnhan

---
+++ Models/ChatMessage.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+namespace DoanVienAPI.Models
+{
+    public class ChatMessage
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string User { get; set; } = string.Empty; // MSSV hoặc tên SV
+        [Required]
+        public string Message { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+        public bool IsRead { get; set; } = false; // Mặc định là chưa đọc
+        public string? Receiver { get; set; }
+    }
+}
---
+++ Models/ChungNhan.cs
@@ -0,0 +1,15 @@
+﻿namespace DoanVienAPI.Models
+{
+    public class ChungNhan
+    {
+        public int Id { get; set; }
+        public string MaChungNhan { get; set; } 
+        public DateTime NgayCap { get; set; }
+
+        public int SinhVienId { get; set; }
+        public virtual SinhVien SinhVien { get; set; }
+
+        public int HoatDongId { get; set; }
+        public virtual HoatDong HoatDong { get; set; }
+    }
+}
