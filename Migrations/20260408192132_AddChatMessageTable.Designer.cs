8914a7a2439ebc51ef99a0f6161a4556359c37ee
thuypy1209 <huymn944@gmail.com>
2026-04-09 09:02:44 +0700

    appdbcontext

--- Migrations/20260408192132_AddChatMessageTable.Designer.cs
+++ Migrations/20260408192132_AddChatMessageTable.Designer.cs
@@ -4,6 +4,7 @@ using DoanVienAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DoanVienAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260408192132_AddChatMessageTable")]
+    partial class AddChatMessageTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
